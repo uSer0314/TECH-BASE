607b7add2b6c1ceb13687dec754df50ac635fec5
uSer0A0 <46949967+uSer0A0@users.noreply.github.com>
2019-10-06 22:11:02 +0900

    Update mission_4-1.php

--- mission_4-1.php
+++ mission_4-1.php
@@ -1,8 +1,8 @@
 ﻿<?php
 	//データベース接続
-	$dsn = 'mysql:dbname=tb210407db;host=localhost';
-	$user = 'tb-210407';
-	$password = 'ZnMkmJhwU7';
+	$dsn = 'データベース名';
+	$user = 'ユーザ名';
+	$password = 'パスワード';
 	$pdo = new PDO($dsn,$user,$password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
 	
 ?>
